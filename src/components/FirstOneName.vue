7c5f19c44277e50edc007f08beec082af4dbccad
刘凯 <850770722@qq.com>
2022-10-23 22:37:42 +0800

    init

---
+++ src/components/FirstOneName.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="hello">
+    {{name}}
+    {{name1}}
+    <p>{{age}}</p>
+    <button @click="plusOne()">+</button>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  name:'FirstOneName',
+  data(){
+    return {
+      name:'xiaosan'
+    }
+  },
+  setup(){
+    const name1 =ref('小四')
+    const age=ref(18)
+    function plusOne(){
+      age.value++ //想改变值或获取值 必须.value
+    }
+    return { //必须返回 模板中才能使用
+      name1,age,plusOne
+    }
+  }
+}
+</script>
